a0c02767ceed17632f9469c2802e52645a31a776
Matheus Azevedo <matheusazevedo85@gmail.com>
2023-07-11 17:24:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using EasyAutomationFramework;
+using OpenQA.Selenium;
+using System;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var web = new Web();
+
+            web.StartBrowser();
+
+            web.Navigate("https://google.com");
+
+            web.AssignValue(TypeElement.Name, "q", "Hello World").element.SendKeys(Keys.Enter);
+
+            web.CloseBrowser();
+        }
+    }
+}
